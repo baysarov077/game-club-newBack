71c79816b1f521b42a5f4d7c709b90f8ef92fe56
MG-LS <gggggg1.97@mail.ru>
2022-04-19 12:31:35 +0300

    EQ

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
+const path = require("path")
 
 app.use(cors())
 app.use(express.json())
+app.use("/image", express.static(path.resolve(__dirname, "image")));
 app.use(require('./routes/users.route'))
 app.use(require('./routes/news.route'))
 app.use(require('./routes/comments.route'))
 app.use(require('./routes/category.route'))
+app.use(require('./routes/equipment.model'))
 
 mongoose
 .connect(process.env.URL)
