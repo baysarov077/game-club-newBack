7ebfd13ace2b932269993e5e427200c9db7fb844
Mac <nahila_95@mail.ru>
2022-04-19 11:43:37 +0300

    add image

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-
-const router = Router();
-app.use(require("./routes/users.route"));
-app.use(require("./routes/news.route"));
-app.use(require("./routes/comments.route"));
-
-module.exports = router;
